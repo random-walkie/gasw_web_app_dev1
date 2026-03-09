ff0fe9228d277d78420cf4c8803cd8b2d68d8743
random-walkie <26143642+random-walkie@users.noreply.github.com>
2026-03-09 11:57:51 +0000

    Update Lab7: defer JavaScript file loading and complete task 1 to log DOM elements and their attributes in documentObjectModel.js
    Signed-off-by: random-walkie <26143642+random-walkie@users.noreply.github.com>

--- lab7/documentObjectModel.js
+++ lab7/documentObjectModel.js
@@ -1,6 +1,33 @@
 "use strict"
-//Finish your task1,2 here
 
+// Task 1
+let newsArticle = document.getElementById("news");
+console.log(newsArticle);
+
+let news_paragraph = document.getElementsByClassName("news_paragraph");
+console.log(news_paragraph)
+
+let news_paragragph_1 = document.getElementsByClassName("news_paragraph")[0];
+console.log(news_paragragph_1);
+
+let all_paragraphs = document.getElementsByTagName("p");
+for (let i = 0; i < all_paragraphs.length; i++) {
+    console.log(all_paragraphs[i].innerHTML);
+}
+
+// Log all child nodes of the body element
+console.log(document.body.childNodes);
+
+// Log the first and last child nodes of the body element
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+
+// Log the next sibling of the first header element
+console.log(document.getElementsByTagName("header")[0].nextElementSibling);
+
+// Log first element with class news_content
+let firstNewsP = document.querySelector(".news_content p");
+console.log(firstNewsP);
 
 let newsContent = document.getElementsByClassName("news_content")[0];
 
